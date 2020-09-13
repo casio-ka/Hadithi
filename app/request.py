a525aa019a60fda0ce8d82e83c90d56186a40bda
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-14 01:20:06 +0300

    updated request to api for sources list

--- app/request.py
+++ app/request.py
@@ -1,4 +1,5 @@
 from app import app
+from models import source
 
 #Getting api key
 api_key = app.config['NEWS_API_KEY']
@@ -7,3 +8,21 @@ source_url = app.config ['NEWS_API_SOURCE_URL']
 #Getting Top Headlines
 category_url = app.config ['NEWS_API_TOP_HEADLINES_URL']
 
+def get_source():
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_source_url = source_url.format(api_key)
+
+    with urllib.request.urlopen(get_source_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_results_list = get_sourcs_response['sources']
+            source_results = process_results(source_results_list)
+
+
+    return source_results
