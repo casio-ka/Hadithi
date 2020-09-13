636ba365c990fdc3454209f78041771f772d72e2
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-14 02:08:04 +0300

    Sources API Call + Fixed 'module' not found error - now working

--- app/request.py
+++ app/request.py
@@ -1,5 +1,8 @@
 from app import app
-from models import source
+import urllib.request,json
+from .models import Source
+
+Source = Source.Source
 
 #Getting api key
 api_key = app.config['NEWS_API_KEY']
@@ -21,8 +24,30 @@ def get_source():
         source_results = None
 
         if get_sources_response['sources']:
-            source_results_list = get_sourcs_response['sources']
+            source_results_list = get_sources_response['sources']
             source_results = process_results(source_results_list)
 
+
+    return source_results
+
+def process_results(source_list):
+    '''
+    function to process the source result and transform them to a list of objects
+    Args:
+        source_list:dictionary containing source details
+    Returns:
+        source_results: A list of source objects
+    '''
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+        if id:
+            source_object = Source(id,name,description,url,category,language)
+            source_results.append(source_object)
 
     return source_results
--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from app import app
+from .request import get_source
 
 @app.route('/')
-@app.route('/home')
 def index():
 
+    #Getting sources
+    source = get_source()
     title = 'Hadithi Hadithi'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, sources = source)
 
