e20a2283ab7939d7d2e9e9d292a90509e0c58cf5
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-14 00:29:35 +0300

    Edited from sources to source for ease of use

--- app/request.py
+++ app/request.py
@@ -3,7 +3,7 @@ from app import app
 #Getting api key
 api_key = app.config['NEWS_API_KEY']
 #Getting Sources url
-sources_url = app.config ['NEWS_API_SOURCES_URL']
+source_url = app.config ['NEWS_API_SOURCE_URL']
 #Getting Top Headlines
 category_url = app.config ['NEWS_API_TOP_HEADLINES_URL']
 
