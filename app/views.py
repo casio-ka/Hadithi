ec5f3e958f3b07e1b633d33e98773708c7130182
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-14 18:36:35 +0300

    Enabled News Sources
    put sources in navbar dropdown , and enabled links

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from app import app
-from .request import get_source
+from .request import get_source, article_source
 
 @app.route('/')
 def index():
@@ -10,3 +10,12 @@ def index():
     title = 'Hadithi Hadithi'
     return render_template('index.html', title = title, sources = source)
 
+@app.route('/article/<id>')
+def article(id):
+
+    '''
+    View article page function that returns the various article details page and its data
+    '''
+    # title= 'Articles'
+    articles = article_source(id)
+    return render_template('article.html',articles= articles,id=id )
